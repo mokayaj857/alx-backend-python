c605fe687d1974e19feb32dc4785f2d68fb512ad
Eng-Prud <prudence.n826@gmail.com>
2024-07-05 23:23:18 +0300

    Increase lines after doc

--- 0-add.py
+++ 0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Module that provides a function to add two floats."""
 
-def add(a: float, b: float) -> float:
+
+
+def add: float, b: float) -> float:
     """Return the sum of two floats a and b."""
     return a + b
 
