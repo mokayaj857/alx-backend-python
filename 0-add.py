a84aeccf830a3a3d4881b996c98c80e86cf5ac93
Eng-Prud <prudence.n826@gmaill.com>
2024-07-05 18:48:14 +0000

    End my task 0 and 1 with a new line

--- 0-add.py
+++ 0-add.py
@@ -7,3 +7,4 @@ float b as arguments and returns their sum as a float.
 def add(a: float, b: float) -> float:
     """Add two floats"""
     return (a + b)
+
--- 1-concat.py
+++ 1-concat.py
@@ -7,3 +7,4 @@ and a string str2 as arguments and returns a concatenated string
 def concat(str1: str, str2: str) -> str:
     """Concatenate two strings"""
     return str1 + str2
+
