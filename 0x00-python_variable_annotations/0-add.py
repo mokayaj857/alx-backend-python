b2466d20155cdc31ce18a5c22aa1fb9f1a2cb99c
Eng-Prud <prudence.n826@gmail.com>
2024-07-08 22:47:51 +0300

    Correctt task0 syntax

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Module that provides a function to add two floats."""
+"""Write a type-annotated function add that takes a float a and a
+float b as arguments and returns their sum as a float.
+"""
 
 
-
-def add: float, b: float) -> float:
-    """Return the sum of two floats a and b."""
-    return a + b
-
+def add(a: float, b: float) -> float:
+    """Add two floats"""
+    return (a + b)
